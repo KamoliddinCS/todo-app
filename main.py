70178a478e7d243ae9213d8c874679fc3e0bc0d3
Kamoliddin <ulbarsovk@gmail.com>
2026-02-07 02:32:36 +0900

    Replaced static self-implemented forms with Flask-WTF-provided forms

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for, session, make_response, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, EmailField, PasswordField
+from wtforms.validators import DataRequired, Email, Regexp, EqualTo
 from werkzeug.utils import secure_filename
+
 import os
+import re
 from config import SECRET_KEY, DEBUG, ADMIN_PASSWORD
 
 
@@ -10,6 +15,25 @@ app.secret_key = SECRET_KEY
 
 UPLOAD_FOLDER = os.path.join(app.root_path, "static", "avatars")
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+
+class LoginForm(FlaskForm):
+    email = EmailField("Email", validators=[Email(check_deliverability=True)])
+    password = PasswordField("Password", validators=[DataRequired()])
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    email = EmailField("Email", validators=[Email(check_deliverability=True)])
+    password = PasswordField("Password", validators=[
+                                                     Regexp(
+                                                         regex=r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9])[^\s]{8,}$',
+                                                         message="Your password has to be at least 8 characters, "
+                                                                 "1 uppercase letter, 1 lowercase letter, 1 digit, "
+                                                                 "1 special character, no spaces."
+                                                     )])
+    confirm = PasswordField("Repeat Password", validators=[EqualTo("password", message="Passwords"
+                                                                                                       "must match.")])
 
 
 class Users:
@@ -46,73 +70,79 @@ def home():
 # AUTHENTICATION LOGIC
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
-    if not session.get("user"):
-        if request.method == "POST":
-            username = request.form["username"]
-            email = request.form["email"]
-            password = request.form["password"]
-            re_password = request.form["re-password"]
-
-            usernames, emails = [i.username for i in users.all.values()], [i.email for i in users.all.values()]
-            print(usernames, emails)
-            if username in usernames or email in emails:
-                flash("This username or email already exists. Please, try again.", "errors")
-                return render_template("signup.html")
-            else:
-                if password != re_password:
-                    flash("Both passwords must be the same.", "errors")
-                    return render_template("signup.html")
-                new_user = User(username, email, password)
-                print("creating a user works")
-                users.all[users.count()+1] = new_user
-                print("adding a user to the users works")
-                session["user"] = {
-                    "username": username,
-                    "email": email
-                }
-                return redirect(url_for("home"))
-        return render_template("signup.html")
-    return redirect(url_for("home"))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        return redirect(url_for("home"))
+    return render_template("signup.html", form=form)
+    # if not session.get("user"):
+    #     if request.method == "POST":
+    #         username = request.form["username"]
+    #         email = request.form["email"]
+    #         password = request.form["password"]
+    #         re_password = request.form["re-password"]
+    #
+    #         usernames, emails = [i.username for i in users.all.values()], [i.email for i in users.all.values()]
+    #         print(usernames, emails)
+    #         if username in usernames or email in emails:
+    #             flash("This username or email already exists. Please, try again.", "errors")
+    #             return render_template("signup.html")
+    #         else:
+    #             if password != re_password:
+    #                 flash("Both passwords must be the same.", "errors")
+    #                 return render_template("signup.html")
+    #             new_user = User(username, email, password)
+    #             print("creating a user works")
+    #             users.all[users.count()+1] = new_user
+    #             print("adding a user to the users works")
+    #             session["user"] = {
+    #                 "username": username,
+    #                 "email": email
+    #             }
+    #             return redirect(url_for("home"))
+    # return redirect(url_for("home"))
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    if not session.get("user"):
-        if request.method == "POST":
-            email = request.form.get("email")
-            password = request.form.get("password")
-
-            if email == "steamkama@gmail.com":
-                if password == ADMIN_PASSWORD:
-                    flash("Welcome, Admin!", "messages")
-                    admin_user = [user for user in users.all.values() if user.email == email][0]
-                    session["user"] = {
-                        "username": admin_user.username,
-                        "email": admin_user.email
-                    }
-                    return redirect(url_for("home"))
-                else:
-                    flash("Invalid admin credentials!", "errors")
-                    return render_template("login.html")
-            else:
-                emails = [user.email for user in users.all.values()]
-                if email not in emails:
-                    flash("A user with this email does not exist. Please, sign up.", "errors")
-                    return render_template("login.html")
-                else:
-                    user = [user for user in users.all.values() if email == user.email][0]
-                    if password != user.password:
-                        flash("Incorrect password! Try again.", "errors")
-                        return render_template("login.html", email=email)
-                    else:
-                        flash("Welcome, bitch!", "messages")
-                        session["user"] = {
-                            "username": user.username,
-                            "email": email
-                        }
-                        return redirect(url_for('home'))
-        return render_template("login.html")
-    return redirect(url_for("home"))
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash("Welcome back!")
+        return redirect(url_for("home"))
+    return render_template("login.html", form=form)
+    # if not session.get("user"):
+    #     if request.method == "POST":
+    #         email = request.form.get("email")
+    #         password = request.form.get("password")
+    #
+    #         if email == "steamkama@gmail.com":
+    #             if password == ADMIN_PASSWORD:
+    #                 flash("Welcome, Admin!", "messages")
+    #                 admin_user = [user for user in users.all.values() if user.email == email][0]
+    #                 session["user"] = {
+    #                     "username": admin_user.username,
+    #                     "email": admin_user.email
+    #                 }
+    #                 return redirect(url_for("home"))
+    #             else:
+    #                 flash("Invalid admin credentials!", "errors")
+    #                 return render_template("login.html")
+    #         else:
+    #             emails = [user.email for user in users.all.values()]
+    #             if email not in emails:
+    #                 flash("A user with this email does not exist. Please, sign up.", "errors")
+    #                 return render_template("login.html")
+    #             else:
+    #                 user = [user for user in users.all.values() if email == user.email][0]
+    #                 if password != user.password:
+    #                     flash("Incorrect password! Try again.", "errors")
+    #                     return render_template("login.html", email=email)
+    #                 else:
+    #                     flash("Welcome, bitch!", "messages")
+    #                     session["user"] = {
+    #                         "username": user.username,
+    #                         "email": email
+    #                     }
+    #                     return redirect(url_for('home'))
 
 
 @app.route("/logout")
